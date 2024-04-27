05c41bf03dbefd94b90c37db1537eeb29612d5f2
sehgxl <abhinavsehgal802@gmail.com>
2024-04-27 19:28:44 +0530

    feat: make svg a component

---
+++ src/pages/Components/LinkArrowTopRight.jsx
@@ -0,0 +1,20 @@
+export default function LinkArrowTopRight() {
+  return (
+    <svg
+      fill="none"
+      height="16"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      width="16"
+      aria-hidden="true"
+      className="ml-1 h-3 w-3"
+    >
+      <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+      <path d="M15 3h6v6"></path>
+      <path d="M10 14L21 3"></path>
+    </svg>
+  );
+}
