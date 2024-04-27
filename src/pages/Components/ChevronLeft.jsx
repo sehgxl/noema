3ed04f1005906f2cfd1cc5efc5308eed4cd422cb
sehgxl <abhinavsehgal802@gmail.com>
2024-04-27 19:27:49 +0530

    feat: add svgs as components

---
+++ src/pages/Components/ChevronLeft.jsx
@@ -0,0 +1,12 @@
+export default function ChevronLeft() {
+  return (
+    <div className="color-[#374151] flex h-2 w-2 items-center justify-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path
+          fill="#374151"
+          d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+        />
+      </svg>
+    </div>
+  );
+}
